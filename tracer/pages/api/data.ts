df46b28813b6bc4c69da44e5c38a5b2fb1806c5a
edwardkwon95 <66803062+edwardkwon95@users.noreply.github.com>
2021-11-29 15:39:04 -0500

    trace

--- tracer/pages/api/data.ts
+++ tracer/pages/api/data.ts
@@ -1,9 +1,8 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { helpers } from '../../data/helpers';
 
-export default function apiHandler(): NextApiHandler {
-  return async (req: NextApiRequest, res: NextApiResponse) => {
+export default function apiHandler(req: NextApiRequest, res: NextApiResponse) {
     const data = helpers.getAll();
+    console.log(data);
     res.status(200).json(data);
-  }
 }
