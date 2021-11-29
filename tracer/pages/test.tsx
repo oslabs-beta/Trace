9893dd1a1e668b0eb4882f0157d134c4c274bf5f
Grace Yun <graceyun@Graces-MacBook-Air.local>
2021-11-28 20:08:26 -0500

    added a test page for folks to view the components they are working on

---
+++ tracer/pages/test.tsx
@@ -0,0 +1,12 @@
+import InnerLayout from "../components/innerlayout"
+
+function Test() {
+  return (
+    <InnerLayout title='test'>
+      {/* insert testing component here */}
+      <a>hey</a>
+    </InnerLayout>
+  )
+}
+
+export default Test
