c268873463ea940e3d4b405d0bb5c56d65b59cac
Alex Hager <alexhager19@gmail.com>
2021-12-01 14:51:37 -0600

    header updates 12/1

--- tracer/pages/test.tsx
+++ tracer/pages/test.tsx
@@ -1,11 +1,21 @@
 import InnerLayout from "../components/innerlayout"
 import Header from "../components/header"
 
+import {
+  FiMenu,
+  FiGrid,
+  FiBarChart,
+  FiAlertTriangle,
+  FiShare2,
+  FiStar,
+  FiSettings
+} from 'react-icons/fi'
+
 function Test() {
   return (
     <InnerLayout title='test'>
       {/* insert testing component here */}
-      <Header text='fjladsfjks' size='lg'/>
+      <Header text='Resolver Metrics' size='md' icon={FiGrid}/>
     </InnerLayout>
   )
 }
