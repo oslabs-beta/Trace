904e69688f8764fa1220526dbf569ad155fda18a
Alex Hager <alexhager19@gmail.com>
2021-12-01 14:51:37 -0600

    header updates 12/1

--- tracer/pages/test.tsx
+++ tracer/pages/test.tsx
@@ -2,12 +2,26 @@ import InnerLayout from "../components/innerlayout"
 import Header from "../components/header"
 import ResolverGraph from "../components/resolverGraph"
 
+import {
+  FiMenu,
+  FiGrid,
+  FiBarChart,
+  FiAlertTriangle,
+  FiShare2,
+  FiStar,
+  FiSettings
+} from 'react-icons/fi'
+
 function Test() {
   return (
     <InnerLayout title='test'>
       {/* insert testing component here */}
+<<<<<<< HEAD
       <Header text='fjladsfjks' size='lg'/>
       <ResolverGraph/>
+=======
+      <Header text='Resolver Metrics' size='md' icon={FiGrid}/>
+>>>>>>> c268873 (header updates 12/1)
     </InnerLayout>
   )
 }
