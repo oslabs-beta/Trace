2eda729ffd5162fdeefe48c7b8a5eb711d2b1c19
Patrick Constant <PLConstant96@gmail.com>
2021-12-01 19:02:55 -0500

    creating modular graphical insights component

--- tracer/pages/test.tsx
+++ tracer/pages/test.tsx
@@ -1,13 +1,14 @@
 import InnerLayout from "../components/innerlayout"
 import Header from "../components/header"
 import ResolverGraph from "../components/resolverGraph"
+import Insights from "../components/insights"
 
 function Test() {
   return (
     <InnerLayout title='test'>
       {/* insert testing component here */}
       <Header text='fjladsfjks' size='lg'/>
-      <ResolverGraph/>
+      {/* <Insights /> */}
     </InnerLayout>
   )
 }
