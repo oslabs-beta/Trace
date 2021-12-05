fb80b25712605447af9fa248658da7885399f123
Grace Yun <graceyunn@gmail.com>
2021-12-04 20:25:09 -0500

    data branch

--- tracer-npm-package/goTrace.js
+++ tracer-npm-package/goTrace.js
@@ -1,7 +1,7 @@
 
 import { parse, validate, execute } from 'graphql';
 import { applyMiddleware } from 'graphql-middleware';
-import { fs } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 const loggingMiddleware = async (resolve, root, args, context, info) => {
   const startTime = process.hrtime();
@@ -14,7 +14,7 @@ const loggingMiddleware = async (resolve, root, args, context, info) => {
 
 module.exports = async function goTrace(schema, query, root, context, variables) {
   // Initial object that will hold all the data we want to send to trace
-  const rootQueryObj = {};
+  const rootQueryObj = { trace_id: uuidv4() };
 
   schema = applyMiddleware(schema, loggingMiddleware);
 
@@ -48,7 +48,7 @@ module.exports = async function goTrace(schema, query, root, context, variables)
       Object.keys(rootQueryObj).includes('errors') ? rootQueryObj['errors'].push(err.message) : rootQueryObj['errors'] = [...err.message];
     });
 
-  //rootQueryObj.totalDuration = JSON.parse((endTime[1] / 1e6).toFixed(2));
+  rootQueryObj.totalDuration = JSON.parse((endTime[1] / 1e6).toFixed(2));
 
   fetch('http://localhost:3000/api/socketio', {
     method: 'POST',
