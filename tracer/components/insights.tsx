98b5aa26a58f19fe9b8e13d8bc858d499f2a1c45
Grace Yun <graceyunn@gmail.com>
2021-12-06 21:53:51 -0500

    scommitting eddys changes

--- tracer/components/insights.tsx
+++ tracer/components/insights.tsx
@@ -1,6 +1,10 @@
 import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+// using chart.js/auto to get started quickly
+// should refactor to import only what is being used
+import Chart from 'chart.js/auto';
+import { Bar } from 'react-chartjs-2';
 
 
 /*
@@ -52,66 +56,160 @@ context = {
     Average Duration of All: ADA
 */
 
+const dummyContext = {
+  TQF: {
+    render: true,
+    list: 4
+  },
+  AQD: {
+    render: true,
+    list: 4
+  },
+  TOF: {
+    render: true,
+    list: 2
+  }
+};
 
-
-const insights = () => {
-
-  const containers = [
-    <GridItem colStart={2} rowSpan={3}  bg='tomato'></GridItem>,
-    <GridItem rowStart={4} rowSpan={3}  bg='tomato'></GridItem>,
-    <GridItem rowStart={4} rowSpan={3}  bg='tomato'></GridItem>
-  ]
-
-  const cardGenerator = (context: any, containers: []) => {
-    const { rawdata, averages, count } = useSelector((state) => state.data);
+const graphs = (context) => {
+  // take in the context object
+  // and create the graphs
+  const { rawdata, averages, count } = useSelector((state) => state.data);
 
     const cards = [];
-    let components = 0  
-
+    
     switch (context.TQF.render) {
       case true:
+        // init the list to a variable
         let j = context.TQF.list;
-        
+        // init an array to be sorted
         const sortable = [];
+        // add objects into that array
         for (const query in count) {
             sortable.push([query, count[query]]);
         }
+        // sort the array in numerical order (descending)
         sortable.sort(function(a, b) {
             return a[1] - b[1];
         });
+        // compile labels & metrics for as many entries as the user selected
+        const labels = [];
+        const metrics = [];
         for (let i = 0; i < j; i++) {
-          // look through the count object and log the 'j' highest value entries
-          const query = sortable[i][0];
-          const freq = sortable[i][1];
-          
+          if (!sortable[i]) break;
+          labels.push(sortable[i][0]);
+          metrics.push(sortable[i][1]);
         }
-
-        // turn tqf into a graph and return that graph in
-        // containers[components];
-
-        components++
-        //useSelector and create     
+      
+      const graphData = {
+        labels,
+        datasets: {
+          label: `Top ${j} Queries by Frequency`,
+          data: metrics,
+          backgroundColor: '#FFFFFF'
+        }
+      }
+        // turn into a graph and return that graph
+      cards.push(<Bar data={graphData} options={}/>)        
+            
     }
 
     switch (context.AQD.render) {
       case true:
-        components++
-        //useSelector and create       
+        let j = context.AQD.list;
+        const sortable = [];
+        for (const query in averages) {
+            sortable.push([query, averages[query]]);
+        }
+        sortable.sort(function(a, b) {
+            return a[1] - b[1];
+        });
+        const labels = [];
+        const metrics = [];
+        for (let i = 0; i < j; i++) {
+          if (!sortable[i]) break;
+          labels.push(sortable[i][0]);
+          metrics.push(sortable[i][1]);
+        }
+
+        const graphData = {
+          labels,
+          datasets: {
+            label: `Top ${j} Queries by Average Duration`,
+            data: metrics,
+            backgroundColor: '#FFFFFF'
+          }
+        }
+
+        cards.push(<Bar data={graphData} options={}/>)        
     }
 
     switch (context.TOF.render) {
       case true:
-        components++
-        //useSelector and create        
+        let j = context.TOF.list;
+        const sortable = [];
+        for (const query in count) {
+          // as this is an operations insight,
+          // we want to filter out all items that
+          // are root queries. Leaving only resolvers
+            if (!rawdata[query]) sortable.push([query, count[query]]);
+        }
+        sortable.sort(function(a, b) {
+            return a[1] - b[1];
+        });
+        const labels = [];
+        const metrics = [];
+        for (let i = 0; i < j; i++) {
+          if (!sortable[i]) break;
+          labels.push(sortable[i][0]);
+          metrics.push(sortable[i][1]);
+        }
+
+        const graphData = {
+          labels,
+          datasets: {
+            label: `Top ${j} Operations by Frequency`,
+            data: metrics,
+            backgroundColor: '#FFFFFF'
+          }
+        };
+
+        cards.push(<Bar data={graphData} options={}/>);        
     }
 
     switch (context.AOD.render) {
       case true:
-        if (components >= 3) return
-        components++
-        //useSelector and create
+        if (cards.length >= 3) return
+        let j = context.AOD.list;
+        const sortable = [];
+        for (const query in averages) {
+            if (!rawdata[query]) sortable.push([query, averages[query]]);
+        }
+        sortable.sort(function(a, b) {
+            return a[1] - b[1];
+        });
+        const labels = [];
+        const metrics = [];
+        for (let i = 0; i < j; i++) {
+          if (!sortable[i]) break;
+          labels.push(sortable[i][0]);
+          metrics.push(sortable[i][1]);
+        }
+
+        const graphData = {
+          labels,
+          datasets: {
+            label: `Top ${j} Operations by Average Duration`,
+            data: metrics,
+            backgroundColor: '#FFFFFF'
+          }
+        };
+
+        cards.push(<Bar data={graphData} options={}/>);          
     }
 
+    /*
+    ::NON-ESSENTIAL INSIGHTS TO ADD LATER::
     switch (context.TFA.render) {
       case true:
         if (components >= 3) return
@@ -125,21 +223,42 @@ const insights = () => {
         components++
         //useSelector and create
     }
-  }
+    */
+    return cards;
+}
+// const graphArray = graphs(context);
 
-
-
-
-
-
-
-
-
+const grid = (graphs) => {
+  // take in the compiled graphs and
+  // return one grid component per graph
+  // (up to 3)
+  const coordProps = [[1, 2], [2, 1], [2, 2]];
+  
+  // do not return components; map them to an array 
+  // inputting the coordProps to each accordingly.
+  // return that array to be passed into insights
   return(
-    <Grid templateColumns='repeat(2, 1fr)' templateRows='repeat(2, 1fr)' h='100%' w='100%' gap={6}>
-      <GridItem colStart={2} rowSpan={1} bg='tomato'></GridItem>
-      <GridItem rowStart={2} rowSpan={1}  bg='tomato'></GridItem>
-      <GridItem rowStart={2} rowSpan={1}  bg='tomato'></GridItem>
+    <GridItem rowStart={coordProps[i][0]} colStart={coordProps[i][1]}>
+
+    </GridItem>
+  )
+
+}
+
+// const gridComponents = grid(graphArray);
+
+const insights = () => {
+// main wrapper for insights page
+// will take in an array of the
+// (up to) three grid components 
+// generated by the grid function
+// and finally, render them.
+  
+  return(
+    <Grid templateColumns='repeat(2, 1fr)' templateRows='repeat(2, 1fr)' h='100%' w='100%' gap={4}>
+      <GridItem colStart={2} bg='tomato'></GridItem>
+      <GridItem rowStart={2} bg='tomato'></GridItem>
+      <GridItem rowStart={2} bg='tomato'></GridItem>
     </Grid>
   )
 }
--- tracer/pages/test.tsx
+++ tracer/pages/test.tsx
@@ -1,5 +1,6 @@
 import InnerLayout from "../components/innerlayout"
 import Header from "../components/header"
+<<<<<<< HEAD
 import ResolverGraph from "../components/resolverMetric"
 
 import {
@@ -11,13 +12,23 @@ import {
   FiStar,
   FiSettings
 } from 'react-icons/fi'
+=======
+import ResolverGraph from "../components/resolverGraph"
+import Insights from "../components/insights"
+>>>>>>> 2eda729 (creating modular graphical insights component)
 
 function Test() {
   return (
     <InnerLayout title='test'>
+<<<<<<< HEAD
 
       <Header text='Resolver Metrics' size='md' icon={FiGrid}/>
 
+=======
+      {/* insert testing component here */}
+      <Header text='fjladsfjks' size='lg'/>
+      {/* <Insights /> */}
+>>>>>>> 2eda729 (creating modular graphical insights component)
     </InnerLayout>
   )
 }
