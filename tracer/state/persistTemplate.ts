375c3591c3ea94fcf337d80a0a74c3773a77d95c
Grace Yun <graceyunn@gmail.com>
2021-12-07 19:33:32 -0500

    persistor

--- tracer/state/persistTemplate.ts
+++ tracer/state/persistTemplate.ts
@@ -1,12 +1,13 @@
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist' // need to install new dependency
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
- 
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from './reducers/dataReducer'
  
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2
 }
  
 const persistedReducer = persistReducer(persistConfig, rootReducer)
