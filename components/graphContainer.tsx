8145a6b4d8e39d706a4e05c176a8f8613b13dd79
Patrick Constant <PLConstant96@gmail.com>
2021-12-10 14:09:32 -0500

    pulling a new branch

--- components/graphContainer.tsx
+++ components/graphContainer.tsx
@@ -2,11 +2,12 @@ import makeGraphs from './graphGenerator'
 import { Grid } from '@chakra-ui/react'
 import { useAppSelector } from '../state/hooks'
 // display grid
+// this file will essentially be the insights component
 
 const insights = () => {
 
   const { rawdata, averages, count} = useAppSelector((state) => state.data)
-
+  
   const graphs = makeGraphs(rawdata, averages, count);
   return (
     <Grid>
