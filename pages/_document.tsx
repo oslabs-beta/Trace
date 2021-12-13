6f87c2c63e210ede97f7aee2c518d7abcaec3a88
Grace Yun <graceyunn@gmail.com>
2021-12-12 21:55:29 -0500

    updated contributing file

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
+      </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Dashboard from '../components/dashboard'
 import Header from '../components/header'
 import { Flex } from '@chakra-ui/layout'
-import Head from 'next/head'
 
 const Home = () => {
 
   return (
     <>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet" />
-      </Head>
       <Flex direction='column' w='90%' h='100vh' p='1.5rem' m='1rem' overflowY='scroll' >
         <Header size='sm' text='Dashboard' />
         <Dashboard />
